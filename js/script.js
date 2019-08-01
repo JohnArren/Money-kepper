279ca08aa87e09976c031c51c1a7c40bcd927108
Kostya <proger007@inbox.ru>
2019-08-01 12:37:42 +0400

    create

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let mbtn = document.getElementById('start'),
+    a = document.querySelector('.budget-value'),
+    b = document.querySelector('.daybudget-value'),
+    c = document.querySelector('.level-value'),
+    d = document.querySelector('.expenses-value'),
+    e = document.querySelector('.optionalexpenses-value'),
+    f = document.querySelector('.income-value'),
+    g = document.querySelector('.monthsavings-value'),
+    k = document.querySelector('.yearsavings-value');
+
+let inp = document.querySelectorAll('.expenses-item');  
+
+let inter1 = document.getElementsByTagName('expenses-item-btn'),
+    inter2 = document.getElementsByTagName('count-budget-btn');
+
+let addition = document.querySelectorAll('expenses-item');   
+
+let c1 = document.querySelector('.choose-income'),
+    c2 = document.querySelector('#savings'),
+    c3 = document.querySelector('#sum'),
+    c4 = document.querySelector('#percent'),
+    c5 = document.querySelector('.year-value'),
+    c6 = document.querySelector('.month-value'),
+    c6 = document.querySelector('.day-value');
+
+
+
+
+
