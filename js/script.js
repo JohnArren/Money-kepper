286b393a56a8798b28246eb1794bede9957854a9
Kostya <proger007@inbox.ru>
2019-08-01 18:51:35 +0400

    replace names and create function

--- js/script.js
+++ js/script.js
@@ -1,27 +1,42 @@
-let mbtn = document.getElementById('start'),
-    a = document.querySelector('.budget-value'),
-    b = document.querySelector('.daybudget-value'),
-    c = document.querySelector('.level-value'),
-    d = document.querySelector('.expenses-value'),
-    e = document.querySelector('.optionalexpenses-value'),
-    f = document.querySelector('.income-value'),
-    g = document.querySelector('.monthsavings-value'),
-    k = document.querySelector('.yearsavings-value');
+let startBtn = document.getElementById('start'),
+    budgetvalue = document.querySelector('.budget-value'),
+    dayBudgetValue = document.querySelector('.daybudget-value'),
+    levelValue = document.querySelector('.level-value'),
+    expensesValue = document.querySelector('.expenses-value'),
+    optionalExpensesValue = document.querySelector('.optionalexpenses-value'),
+    incomeValue = document.querySelector('.income-value'),
+    monthSavingsValue = document.querySelector('.monthsavings-value'),
+    yearSavingsValue = document.querySelector('.yearsavings-value'),
 
-let inp = document.querySelectorAll('.expenses-item');  
+    expensesItem = document.querySelectorAll('.expenses-item'),
+    expensesBtn = document.getElementsByTagName('button')[0],
+    optionalExpensesBtn = document.getElementsByTagName('button')[1],
+    countBtn = document.getElementsByTagName('button')[2],
+    optionalExpensesItem = document.querySelectorAll('optionalexpenses-item'),
 
-let inter1 = document.getElementsByTagName('expenses-item-btn'),
-    inter2 = document.getElementsByTagName('count-budget-btn');
 
-let addition = document.querySelectorAll('expenses-item');   
+    incomeItem = document.querySelector('.choose-income'),
+    checkSavings = document.querySelector('#savings'),
+    sumValue = document.querySelector('#sum'),
+    percentValue = document.querySelector('#percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
 
-let c1 = document.querySelector('.choose-income'),
-    c2 = document.querySelector('#savings'),
-    c3 = document.querySelector('#sum'),
-    c4 = document.querySelector('#percent'),
-    c5 = document.querySelector('.year-value'),
-    c6 = document.querySelector('.month-value'),
-    c6 = document.querySelector('.day-value');
+let money, time;
+
+function start(){
+
+    money = +prompt("Ваш бюджет на месяц ?",'');
+    time = prompt("Введите дату в формате YYYY-MM-DD",'');
+
+    while (isNaN(money) || money == '' || money == null ){
+        money = prompt("Ваш бюджет", "");
+    }
+}
+
+start();
+
 
 
 
